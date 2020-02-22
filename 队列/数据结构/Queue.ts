87e1c54514507d26d00ad73c15e2178d41222b4f
nfwyst <nfwyst@gmail.com>
2020-02-22 19:54:28 +0800

    update

---
+++ 队列/数据结构/Queue.ts
@@ -0,0 +1,42 @@
+// 节点
+class QueueNode {
+  public next: QueueNode | null = null
+  constructor(public value: any) { }
+}
+
+// 队列
+class Queue {
+  public first: QueueNode | null = null
+  public last: QueueNode | null = null
+  public length = 0
+
+  // 获取头部
+  public peek(): QueueNode | null {
+    return this.first
+  }
+
+  // 入队列
+  public enqueue(value: any): Queue {
+    const node = new QueueNode(value)
+    if (!this.length) {
+      this.first = node
+      this.last = node
+    } else {
+      if (this.last) this.last.next = node
+      this.last = node
+    }
+    this.length++
+    return this
+  }
+
+  // 出队列
+  public dequeue(): any {
+    const item = this.first
+    if (!item || !this.first) return item
+    this.first = this.first.next
+    this.length--
+    return item.value
+  }
+}
+
+export default Queue
