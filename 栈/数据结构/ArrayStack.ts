b9b8cc6cf9569a0d3e47117a8660a20106e2b2c0
nfwyst <nfwyst@gmail.com>
2020-02-22 19:41:22 +0800

    update

--- 栈/数据结构/ArrayStack.ts
+++ 栈/数据结构/ArrayStack.ts
@@ -1,15 +1,18 @@
 class ArrayStack {
   public array: any[] = []
 
+  // 返回栈顶
   public peek(): any {
     return this.array[this.array.length - 1]
   }
 
+  // 入栈
   public push(value: any): ArrayStack {
     this.array.push(value)
     return this
   }
 
+  // 出栈
   public pop(): any {
     return this.array.pop()
   }
