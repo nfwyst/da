1b700535b420ff10642ba24edeffad0b74df4c57
nfwyst <nfwyst@gmail.com>
2020-02-23 07:16:27 +0800

    update

--- 树/二叉树/BinarySearchTree.ts
+++ 树/二叉树/BinarySearchTree.ts
@@ -49,6 +49,34 @@ class BinarySearchTree {
     }
     return
   }
+
+  // 删除节点 TODO
+  public remove(value: any): boolean {
+    // 如果是空树则返回
+    if (!this.root) return
+    // 当前节点
+    let currentNode = this.root
+    // 父节点
+    let parentNode = null
+    // 如果有节点可处理
+    while (currentNode) {
+      if (value < currentNode.value) {
+        // 要删除的节点在左侧
+      } else if (value > currentNode.value) {
+        // 要删除的节点在右侧
+      } else {
+        // 当前节点需要被删除
+        if (currentNode.right === null) {
+          // 当前节点没有右子树
+        } else if (currentNode.right.left === null) {
+          // 当前节点有右子树， 但右子树没有左子树
+        } else {
+          // 当前节点有右子树， 且右子树有左子树
+        }
+      }
+    }
+    return true
+  }
 }
 
 export default BinarySearchTree
